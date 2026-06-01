dae0c52037e1c8544bb667a3484a6fc85ad772e7
Aadam Jacobs <142569919+SordidMovie4905@users.noreply.github.com>
2026-06-01 15:32:25 +0200

    Add files via upload

---
+++ loops and stuff.js
@@ -0,0 +1,79 @@
+// TODO: Create a function called sum. The function will take in a parameter andcalculate all the numbers from 0 -> the parameter. You must check if the parameterisan integer first before any calculation is made. If the parameter is not a number, return a message stating, “The value passed is not a number”. You are NOT allowedtouse methods(!Number.isInteger(n)) or regular expressions(.match(/cat/g)). Makesureto test your code effectively. 
+function sum(n) {
+    if (typeof n !== "number" || n % 1 !== 0) {
+        return "The value passed is not a number";
+    }
+
+    let total = 0;
+
+    for (let i = 0; i <= n; i++) {
+        total += i;
+    }
+
+    return total;
+}
+
+console.log(sum(5));
+console.log(sum("5"));
+// TODO: Create a function called ‘factorial’ that takes in a number as a parameter. The function will print the factorial of the entered number, e.g. factorial(4) ->4*3*2*1 //output 24
+function factorial(n) {
+    if (typeof n !== "number" || n < 0 || n % 1 !== 0) {
+        return "Invalid number";
+    }
+
+    let result = 1;
+
+    for (let i = n; i > 0; i--) {
+        result *= i;
+    }
+
+    return result;
+}
+
+console.log(factorial(4));
+// TODO: Create a function called funkyMath . If this function is calledwith2arguments the function will subtract the first from the second. If the functionis calledwith 3 arguments it will add all 3 numbers together. If the function is calledwith4arguments it will add together argument 1 and 2 , 3 and 4 separately. Thendividethem accordingly, eg funkyMath(8,2,3,5) -> 8+2 divided by 3+5 -> 10/8 //output 1,25
+function funkyMath(a, b, c, d) {
+
+    if (arguments.length === 2) {
+        return a - b;
+    }
+
+    if (arguments.length === 3) {
+        return a + b + c;
+    }
+
+    if (arguments.length === 4) {
+        return (a + b) / (c + d);
+    }
+
+    return "Invalid number of arguments";
+}
+
+console.log(funkyMath(10, 4));
+console.log(funkyMath(1, 2, 3));
+console.log(funkyMath(8, 2, 3, 5));
+// TODO: Create a loop that will remove all the odd numbers fromthe array andaddthem to a new array. Use the current array [1, 2 , 33, 45, 6,44]. Bonus: Make sure to arrange them from smallest to biggest.()
+let arr = [1, 2, 33, 45, 6, 44];
+let evenNumbers = [];
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+        evenNumbers.push(arr[i]);
+    }
+}
+
+evenNumbers.sort((a, b) => a - b);
+
+console.log(evenNumbers);
+// TODO: Create an object called ‘me’ with properties of first name, last name, age, favourite colour, dream car
+let me = {
+    firstName: "Aadam",
+    lastName: "Jacobs",
+    age: 23,
+    favouriteColour: "Blue",
+    dreamCar: "Nissan GTR R34 Skyline V-Spec II"
+};
+// TODO: Create and add a new property and value of ‘favourite food’ to the object. 
+me.favouriteFood = "Pizza";
+// TODO: . Now delete the age property from the object.
+delete me.age;
